373a330299afad2008db7ca655458b4a12539196
IuriVeloso <iuriv.felix@gmail.com>
2024-08-09 08:14:00 -0300

    feat: add get list or 404 functions

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,6 +6,8 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.home, name="home"),  # Home
-    path('recipes/<int:id>/', views.recipes, name="recipe"),  # Home
+    path('recipes/<int:id>/', views.recipe, name="recipe"),  # Home
+    path('recipes/category/<int:category_id>/',
+         views.category, name="category"),  # Home
 
 ]

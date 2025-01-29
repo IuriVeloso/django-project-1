c20d502a4ae50ba8562fb6ab6220851eea9f2954
IuriVeloso <iuriv.felix@gmail.com>
2025-01-29 09:14:57 -0300

    test: add test

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -11,7 +11,32 @@ class AuthorRegisterFormUnitTest(TestCase):
         ('password', 'Type your password'),
         ('password2', 'Repeat your passworld'),
     ])
-    def test_first_name_placeholder_is_correct(self, field, placeholder):
+    def test_fields_placeholder(self, field, placeholder):
         form = RegisterForm()
         field_placeholder = form[field].field.widget.attrs['placeholder']
         self.assertEqual(placeholder, field_placeholder)
+        
+    @parameterized.expand([ 
+        ('username', 'Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        ('email', 'The e-mail must be valid.'),
+        ('password',  'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'),
+    ])
+    def test_fields_help_text(self, field, needed):
+        form = RegisterForm()
+        current = form[field].field.help_text
+        self.assertEqual(current, needed)
+        
+    @parameterized.expand([ 
+        ('username', 'Username'),
+        ('first_name', 'First name'),
+        ('last_name', 'Last name'),
+        ('email', 'E-mail'),
+        ('password',  'Password'),
+        ('password2',  'Password2'),
+    ])
+    def test_fields_label(self, field, needed):
+        form = RegisterForm()
+        current = form[field].field.label
+        self.assertEqual(current, needed)

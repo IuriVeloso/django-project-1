7e0b62ab745f81433af3a0f8b7ac328f738a99ac
IuriVeloso <iuriv.felix@gmail.com>
2025-05-04 14:46:28 -0300

    feat: add base authors test

---
+++ tests/functional_tests/authors/base.py
@@ -0,0 +1,16 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from utils.browser import make_firefox_browser
+import time
+
+class AuthorsBaseTest(StaticLiveServerTestCase):
+    def setUp(self):
+        self.browser = make_firefox_browser()
+        return super().setUp()
+    
+    def tearDown(self):
+        self.browser.quit()
+        return super().tearDown()
+    
+    def sleep(self, qtd=5):
+        time.sleep(qtd)
+        

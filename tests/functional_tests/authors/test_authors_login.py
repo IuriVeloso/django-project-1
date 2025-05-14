3d147768595a0db39135992a27de4910eccdb0a3
IuriVeloso <iuriv.felix@gmail.com>
2025-05-14 16:06:55 -0300

    feat: add logout tet

--- tests/functional_tests/authors/test_authors_login.py
+++ tests/functional_tests/authors/test_authors_login.py
@@ -24,9 +24,31 @@ class AuthorsLoginTest(AuthorsBaseTest):
         
         form.submit()
         
-        self.sleep(5)
-        
+        self.sleep(1)
+                
         self.assertIn(f'You are logged in with {user.username}.', 
                         self.browser.find_element(By.TAG_NAME, 'body').text)
+        
+    def test_login_create_raises_404_if_not_POST_method(self):
+        self.browser.get(self.live_server_url + reverse('authors:login-create'))
+
+        self.assertIn('Not Found', self.browser.find_element(By.TAG_NAME, 'body').text)
+    
+    def test_form_login_is_invalid(self):
+        self.browser.get(self.live_server_url + reverse('authors:login'))
+
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+
+        username = self.get_by_placeholder(form, 'Type your username')
+        password = self.get_by_placeholder(form, 'Type your password')
+        
+        username.send_keys('  ')
+        password.send_keys('  ')
+        
+        form.submit()
+        self.sleep(1)
+        
+        self.assertIn('Invalid username or password!', self.browser.find_element(By.TAG_NAME, 'body').text)
+        
         
         
